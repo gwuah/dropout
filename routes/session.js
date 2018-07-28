dedda974c2231afc493c48862be82531805cb714
Casprine Assempah <casprine.001@gmail.com>
2018-07-28 12:42:58 +0000

    minor changes

--- routes/session.js
+++ routes/session.js
@@ -1,44 +1,46 @@
-var express = require('express');
-var passport = require('passport');
-var Session = require('../models/Session');
+var express = require("express");
+var passport = require("passport");
+var Session = require("../models/Session");
 var router = express.Router();
 
-router.get('/create-session', (req, res) => {
+router.get("/create-session", (req, res) => {
   let session = new Session();
 
   session.save(function(err, data) {
-    if (err) { 
+    if (err) {
       console.log(err);
-      res.render('error')
+      res.render("error");
     } else {
-      res.redirect('/session/' + data._id)
+      res.redirect("/session/" + data._id);
     }
-  })
-})
+  });
+});
 
-router.get('/session/:id', (req, res) => {
+router.get("/session/:id", (req, res) => {
+  console.log(res.user);
+
   if (req.params.id) {
-    Session.findOne({_id: req.params.id }, (err, data) => {
+    Session.findOne({ _id: req.params.id }, (err, data) => {
       if (err) {
         console.log(err);
-        res.render('error')
+        res.render("error");
       }
 
       if (data) {
-        Session.find({_id: req.params.id}, (err, data) => {
-          console.log(data)
-          res.render('session', {
-            code: data.code , 
+        Session.find({ _id: req.params.id }, (err, data) => {
+          console.log(data);
+          res.render("session", {
+            code: data.code,
             sessionId: data._id
-          })
-        })
+          });
+        });
       } else {
-        res.render('error')
+        res.render("error");
       }
-    })
+    });
   } else {
-    res.render('error')
+    res.render("error");
   }
-})
+});
 
 module.exports = router;
