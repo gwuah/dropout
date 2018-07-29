36fb04e7fe51c58dd62311d3a9d1f08c07ad70da
Nana Adjei Manu <claeusdev@gmail.com>
2018-07-29 10:00:30 +0000

    remove mailer config

--- config.js
+++ config.js
@@ -1,13 +1,7 @@
 'use strict'
 
 module.exports = {
-    mailer: {
-        service: 'Gmail',
-        auth: {
-            user: 'has.hamza247@gmail.com',
-            pass: 'anumshaikh2017'
-        }
-    },
+    
     dbConnstring: 'mongodb://gwuah:gwuah2018@ds253871.mlab.com:53871/watcher',
     sessionKey: 'HaloCode4Share'
 }
